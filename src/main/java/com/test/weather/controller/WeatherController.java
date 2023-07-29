a9e44a55a47f8a4730fe47da4679a3e72283fc49
can <oz.azizcann@gmail.com>
2023-07-29 20:24:48 +0300

    city name validation added

--- src/main/java/com/test/weather/controller/WeatherController.java
+++ src/main/java/com/test/weather/controller/WeatherController.java
@@ -1,15 +1,20 @@
 package com.test.weather.controller;
 
+import com.test.weather.controller.validation.CityNameConstraint;
 import com.test.weather.dto.WeatherDto;
 import com.test.weather.service.WeatherService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Validated
 @RestController
 @RequestMapping("/v1/weather")
 public class WeatherController {
@@ -17,7 +22,7 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping("/{city}")
-    public ResponseEntity<WeatherDto> getWeather(@PathVariable String city) {
+    public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") @CityNameConstraint @NotBlank String city) {
         return ResponseEntity.ok(weatherService.getWeatherByCityName(city));
     }
 }
